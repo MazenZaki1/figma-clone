97ce23f6e6a4d103a345bfbd3e124418f672c9a7
Mazen Ahmed Zaki <mazennzakii@gmail.com>
2025-04-11 20:57:34 +0200

    added signin page

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import Link from "next/link";
+import { useActionState } from "react";
+import { authenticate } from "../actions/auth";
+
+export default function Page() {
+    const [errorMessage, formAction, isPending] = useActionState(
+    authenticate,
+    undefined,
+    );
+
+    return (
+      <div className="flex min-h-screen items-center justify-center bg-gray-100 px-4">
+        <div className="w-full max-w-sm space-y-6">
+          <h1 className="text-center text-6xl font-semibold">Sign In</h1>
+          <h3 className="text-center text-sm font-semibold text-gray-600">
+            Welcome back! Please enter your details.
+          </h3>
+          <form action={formAction} className="space-y-4">
+            <input type="hidden" name="redirectTo" value="/dashboard" />
+            <div className="flex flex-col space-y-2">
+              <label htmlFor="email" className="text-sm font-medium">
+                Email
+              </label>
+              <input
+                // type="email"
+                name="email"
+                className="rounded-md border-gray-300 py-1 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                // required
+              />
+            </div>
+            <div className="flex flex-col space-y-2">
+              <label htmlFor="password" className="text-sm font-medium">
+                Password
+              </label>
+              <input
+                // type="password"
+                name="password"
+                // minLength={8}
+                className="rounded-md border-gray-300 py-1 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                // required
+              />
+            </div>
+            <div className="flex flex-row items-center">
+              <input
+                type="checkbox"
+                name="rememberMe"
+                className="rounded-md border-gray-300 py-1 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+              />
+              <label
+                htmlFor="rememberMe"
+                className="ml-1.5 text-sm font-medium"
+              >
+                Remember Me?
+              </label>
+            </div>
+            <button
+                        type="submit"
+            disabled={isPending}
+              className="w-full rounded-md bg-blue-600 py-2 font-semibold text-white hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
+            >
+            {isPending ? "Signing In..." : "Sign In"}
+            </button>
+            <p className="text-center text-xs text-gray-600">
+              Don&apos;t have an account?{" "}
+              <Link className="text-blue-600 hover:underline" href="/signup">
+                Sign Up
+              </Link>
+            </p>
+            {errorMessage && (
+              <p className="text-center text-sm text-red-500">{errorMessage}</p>
+            )}
+          </form>
+        </div>
+      </div>
+    );
+}
